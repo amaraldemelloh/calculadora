a613a157b03b72902f6d0ac642b2cb329ff10703
amaraldemelloh <henrique.amaraldemello@pm.me>
2022-02-04 16:01:43 -0300

    Exibirá o resultado das operações na tela por função eval

--- js/script.js
+++ js/script.js
@@ -1,9 +1,16 @@
-const res = document.querySelector('#res');
 const numbersDiv = document.querySelector('#numbers');
+const res = document.querySelector('#res');
 
 for (let i = 0; i < 10; i++) {
     let number = document.createElement('button');
     number.className = 'numbersBtn';
     number.innerText = i;
+    number.onclick = function () {
+        res.innerText += i;
+    };
     numbersDiv.appendChild(number);
+};
+
+function calc() {
+    res.innerText = eval(res.innerText);
 };
