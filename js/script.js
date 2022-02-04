75e16355bb28992041ff93c81289b48c3064217b
amaraldemelloh <henrique.amaraldemello@pm.me>
2022-02-04 15:39:28 -0300

    Adicionará botões numéricos

--- js/script.js
+++ js/script.js
@@ -1,1 +1,9 @@
-const res = document.getElementById('res');
+const res = document.querySelector('#res');
+const numbersDiv = document.querySelector('#numbers');
+
+for (let i = 0; i < 10; i++) {
+    let number = document.createElement('button');
+    number.className = 'numbersBtn';
+    number.innerText = i;
+    numbersDiv.appendChild(number);
+};
